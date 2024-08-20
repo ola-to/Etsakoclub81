55d084432d33d6cd35a8b8ee012c137d4464b43d
ola-to <oluwatosin2r2o@gmail.com>
2024-08-20 10:44:53 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
             <a href="register.php">Register</a>
             <a href="login.php">Login</a>
         <?php endif; ?>
-        <a href="index.php">Back to Main Website</a>
+        <a href="etsakoclub81.org">Back to Main Website</a>
         <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
             <a href="logout.php">Logout</a>
         <?php endif; ?>
@@ -39,7 +39,7 @@
             <a href="register.php">Register</a>
             <a href="login.php">Login</a>
         <?php endif; ?>
-        <a href="index.php">Back to Main Website</a>
+        <a href="etsakoclub81.org">Back to Main Website</a>
         <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
             <a href="logout.php">Logout</a>
         <?php endif; ?>
