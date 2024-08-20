2412bd4f2707eeda0905c8beedc255a016651f0f
ola-to <oluwatosin2r2o@gmail.com>
2024-08-20 14:55:42 +0100

    Update forgot_password_process.php

--- forgot_password_process.php
+++ forgot_password_process.php
@@ -30,7 +30,7 @@ if ($result->num_rows > 0) {
     $sql = "UPDATE users SET reset_token='$token', reset_requested_at=NOW() WHERE email='$email'";
     if ($conn->query($sql) === TRUE) {
         // Send reset email
-        $resetLink = "https://directory.etsakoclub81.org/reset_password.php?token=" . $token;
+        $resetLink = "reset_password.php?token=" . $token;
         $subject = "Password Reset Request";
         $message = "Click on the following link to reset your password: " . $resetLink;
         $headers = "From: no-reply@domain.com";
