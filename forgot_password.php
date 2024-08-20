9b3b2bdc2613b44d741ee1099e8a51b9609069be
ola-to <oluwatosin2r2o@gmail.com>
2024-08-20 14:11:15 +0100

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -8,43 +8,7 @@
     <title>Forget password</title>
 </head>
 <body>
-    <div class="header">
-        <a href="#" class="logo">
-            <img src="ets.png" alt="Logo">
-        </a>
-        <div class="search-box">
-            <input type="text" id="search-input" placeholder="Search....">
-            <button id="search-button" style="background-color: green; color: white;" onclick="performSearch()">🔍</button>
-        </div>
-        <div class="business-links">
-            <a href="https://directory.etsakoclub81.org">Home</a>
-            <a href="https://directory.etsakoclub81.org/register.html">Register</a>
-            <a href="https://directory.etsakoclub81.org/login.html">Login</a>
-            <a href="https://directory.etsakoclub81.org">Back to Main Website</a>
-        </div>
-    </div>
-    <div class="mobile-header">
-        <div class="mobile-search-box">
-            <input type="text" placeholder="" aria-label="Search">
-        </div>
-        <a href="#" class="logo">
-            <img src="ets.png" alt="Logo">
-        </a>
-        <button class="menu-icon" onclick="openPopup()">☰</button>
-    </div>
-    <div class="searchm-box">
-            <input type="text" id="search-input" placeholder="Search... 🔍">
-            <button id="search-button" style="background-color: green; color: white;" onclick="performSearch()">🔍</button>
-        </div>
-    <div class="popup" id="menuPopup">
-        <div class="popup-content">
-            <a href="https://directory.etsakoclub81.org">Home</a>
-            <a href="https://directory.etsakoclub81.org/register.html">Register</a>
-            <a href="https://directory.etsakoclub81.org/login.html">Login</a>
-            <a href="https://directory.etsakoclub81.org">Back to Main Website</a>
-            <button class="close-btn" onclick="closePopup()">Close</button>
-        </div>
-    </div>
+    <?php include 'includes/header.php'; ?>
     <div class="subheader"></div>
     <div class="content">
         <div class="form-header">
